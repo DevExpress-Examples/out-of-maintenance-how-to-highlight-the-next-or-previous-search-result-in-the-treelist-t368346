bdf36b8fcb97016494b61f5e79ca5b2937fbfe1a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ CS/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.XtraTreeList.Columns;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetDataSource();
+        }
+
+        private void SetDataSource()
+        {
+            DataSet set = new DataSet();
+            set.ReadXml(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\Contacts.xml");
+            treeList1.DataSource = set.Tables[0];
+            treeList1.Columns["Description"].Visible = false;
+            treeList1.BestFitColumns();
+        }
+    }
+}
